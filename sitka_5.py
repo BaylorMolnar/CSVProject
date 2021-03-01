90deb30e12f18a7fdac6b53eb7bd4698b1a45385
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-02-28 23:03:56 -0600

    Correctly Formatted Final Chart

--- sitka_5.py
+++ sitka_5.py
@@ -23,6 +23,7 @@ lows = []
 tmax_index = header_row.index("TMAX")
 tmin_index = header_row.index("TMIN")
 date_index = header_row.index("DATE")
+dv_title_index = header_row.index("NAME")
 
 for row in csv_file:
     try:
@@ -35,6 +36,7 @@ for row in csv_file:
         highs.append(int(row[tmax_index]))
         lows.append(int(row[tmin_index]))
         dates.append(converted_date)
+        dv_chart_title = row[dv_title_index]
 
 
 # print(highs)
@@ -62,6 +64,7 @@ lows2 = []
 tmax_index2 = header_row2.index("TMAX")
 tmin_index2 = header_row2.index("TMIN")
 date_index2 = header_row2.index("DATE")
+sitka_title_index = header_row2.index("NAME")
 # use enumerate function instead of hardcoding row value
 
 for row in csv_file2:
@@ -69,22 +72,35 @@ for row in csv_file2:
     converted_date = datetime.strptime(row[date_index2], "%Y-%m-%d")
     dates2.append(converted_date)
     lows2.append(int(row[tmin_index2]))
+    chart_title = row[sitka_title_index]
 
+import numpy as np
+
+ytix = np.arange(30, 71, 10)
+ytix2 = np.arange(40, 121, 20)
 
 fig3, ax = plt.subplots(2)
 ax[0].plot(dates2, highs2, c="red")
 ax[0].plot(dates2, lows2, c="blue")
 ax[0].fill_between(dates2, highs2, lows2, facecolor="blue", alpha=0.1)
-ax[0].set_title("Sitka- Daily High and Low Temperatures - 2018", fontsize=16)
-ax[0].set_xlabel("", fontsize=12)
-ax[0].set_ylabel("Temperature(F)", fontsize=12)
+ax[0].set_title(chart_title, fontsize=16)
+# ax[0].set_xlabel("", fontsize=12)
+# ax[0].set_ylabel("Temperature(F)", fontsize=12)
 ax[0].tick_params(axis="both", labelsize=16)
+ax[0].axes.xaxis.set_ticklabels([])
+ax[0].set_yticks(ytix)
 
 ax[1].plot(dates, highs, c="red")
 ax[1].plot(dates, lows, c="blue")
 ax[1].fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
-ax[1].set_title("Death Valley- Daily High and Low Temperatures - 2018", fontsize=16)
-ax[1].set_xlabel("", fontsize=12)
-ax[1].set_ylabel("Temperature(F)", fontsize=12)
+ax[1].set_title(dv_chart_title, fontsize=16)
+# ax[1].set_xlabel("", fontsize=12)
+# ax[1].set_ylabel("Temperature(F)", fontsize=12)
 ax[1].tick_params(axis="both", labelsize=16)
+ax[1].set_yticks(ytix2)
+plt.xticks(rotation=45)
+fig3.suptitle(
+    f"Temperature comparison between {chart_title} and {dv_chart_title}", fontsize=16
+)
+fig3.tight_layout()
 plt.show()
